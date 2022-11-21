0c61964c591c5a108b6988728dad552baa44a3af
ixNoah <70314622+NoahTheNerd@users.noreply.github.com>
2022-11-21 17:40:18 +0100

    wtf_is_this

--- src/other.js
+++ src/other.js
@@ -2,8 +2,6 @@ import U from '../resources/util'
 import Settings from '../resources/config'
 import request from 'requestV2';
 
-request('http://dev.ixnoah.live:7777/')
-
 register('chat', (message, event) => {
 	if (U.inHousing() && Settings.gui_actionMsg) {
 		ChatLib.chat(message)
