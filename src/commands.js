fb623b2182b34cde39ebdbbbea094d007e491e69
ixNoah <70314622+NoahTheNerd@users.noreply.github.com>
2022-11-25 17:45:48 +0100

    Bookmark work properly

--- src/commands.js
+++ src/commands.js
@@ -12,18 +12,19 @@ register('command', () => {FileLib.write('ixMod', 'housetracker/data.json', "[]"
 
 if (Settings.cmd_stats) register('command', () => {if (U.inHousing()) { ChatLib.command(`viewstats ${Player.getName()}`) }}).setName('mystats')
 if (Settings.cmd_unicode) register('command', () => {ChatLib.chat(`&9&m${'-'.repeat(52)}`);UniTexts.forEach(txt => ChatLib.chat(txt));ChatLib.chat(`&9&m${'-'.repeat(52)}`)}).setName('unicode').setAliases(['uni']) 
+if (Settings.cmd_unbreakable) {register('command', () => { if (Player.getHeldItem()) { 
+	if (!U.isCreative()) return ChatLib.chat('&cYou need to be in Creative mode for this command!')
+	U.setHeldItemTag('Unbreakable', 1)
+}}).setCommandName('unbreakable')} 
+
+
+// Bookmark
 
 // Ive been working on this hell all day put me out of my misery
-
-if (Settings.cmd_bookmark) register('command', (player) => {
+if (Settings.cmd_bookmark) register('command', (player, target) => {
 	if (player && player.startsWith('-r')) { // this is bullshit but it doesn't work otherwise
-		let v = U.popFile('resources/bookmark.json', 'id', player.split(' ')[1])
-		if (v==200) {
-			ChatLib.chat('&cSuccessfully removed this bookmark!')
-		} else {
-			ChatLib.chat('&cSomething went wrong trying to perform this action!')
-		}
-		
+		let v = U.popFile('resources/bookmark.json', 'id', target)
+		if (v==200) {ChatLib.chat('&cSuccessfully removed this bookmark!')} else {ChatLib.chat('&cSomething went wrong trying to perform this action!')}
 		return;
 	}
 	if (player && player.match(/^[a-zA-Z0-9_]{2,16}$/mg)) {
@@ -36,22 +37,17 @@ if (Settings.cmd_bookmark) register('command', (player) => {
 	} else if (!player && U.inHousing(true)) {
 		const Bookmarks = JSON.parse(FileLib.read('ixMod', 'resources/bookmark.json'))
 		let BookmarkMsg = []
-		ChatLib.chat(`&8&m${'-'.repeat(52)}`)
+		if (Bookmarks.length<1) return ChatLib.chat('&cYou have no bookmarks! Type /bookmark <player> to bookmark a house!')
+		ChatLib.chat(`&9&m${'-'.repeat(32)}`)
 		Bookmarks.forEach(v => {
 			let message = new Message()
 			message.addTextComponent(new TextComponent('&c[X] ').setClick('run_command', `/bookmark -r ${v.id}`).setHover('show_text',`&cRemove bookmark "${v.name}"`))
-			message.addTextComponent(new TextComponent(`&8|&7 ${v.name}`).setHover('show_text',`&aVisit bookmark "${v.name}"`).setClick('run_command', `/visit ${U.addStr(U.addStr(U.addStr(U.addStr(v.id, 8, '-'), 13, '-'), 18, '-'), 23, '-')}`))
+			message.addTextComponent(new TextComponent(`&8|&e ${v.name}`).setHover('show_text',`&aVisit bookmark "${v.name}"`).setClick('run_command', `/visit ${U.addStr(U.addStr(U.addStr(U.addStr(v.id, 8, '-'), 13, '-'), 18, '-'), 23, '-')}`))
 			ChatLib.chat(message)
 		})
-		ChatLib.chat(`&8&m${'-'.repeat(52)}`)
+		ChatLib.chat(`&9&m${'-'.repeat(32)}`)
 	} else if (!player && U.inHousing(true)==false) {
 		ChatLib.chat('&cYou need to be in Housing or a Housing lobby to use this command!')
 	}
 }).setName('bookmark')
 register('command', () => {console.log(Player.getHeldItem().getRawNBT())}).setName('yoink')
-
-
-if (Settings.cmd_unbreakable) {register('command', () => { if (Player.getHeldItem()) { 
-	if (!U.isCreative()) return ChatLib.chat('&cYou need to be in Creative mode for this command!')
-	U.setHeldItemTag('Unbreakable', 1)
-}}).setCommandName('unbreakable')} 
