7620c6afac990da449091b2730130bc16a662edf
ixNoah <70314622+NoahTheNerd@users.noreply.github.com>
2022-12-08 09:42:07 +0100

    Update other.js

--- src/other.js
+++ src/other.js
@@ -8,10 +8,13 @@ register('chat', (message, event) => {
 	}
 }).setCriteria('&r&7* ${message}')
 
-let act = { x: 0, y: 0, z: 0 } // Putting this in an object in case anything else may need this
+let currentCoordinates = { x: 0, y: 0, z: 0 } // Putting this in an object in case anything else may need this
 register('renderOverlay', () => {
-	act.x=Player.getX().toFixed(0);act.y=Player.getY().toFixed(0);act.z=Player.getZ().toFixed(0)
+	currentCoordinates.x=Player.getX().toFixed(0);
+	currentCoordinates.y=Player.getY().toFixed(0);
+	currentCoordinates.z=Player.getZ().toFixed(0)
+	
 	if (Settings.gui_xyzhotbar && U.inHousing()) {
-		ChatLib.actionBar(`&b${act.x}&8 | &b${act.y}&9/195 &8|&b ${act.z}`)
+		ChatLib.actionBar(`&b${currentCoordinates.x}&8 | &b${currentCoordinates.y}&9/195 &8|&b ${currentCoordinates.z}`)
 	}
 })
