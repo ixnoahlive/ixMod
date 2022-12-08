499a2bc9be9d8946da52ff2c36b663c7079c172a
ixNoah <70314622+NoahTheNerd@users.noreply.github.com>
2022-12-08 09:49:00 +0100

    Variable Clarity

--- src/cmds/bookmark.js
+++ src/cmds/bookmark.js
@@ -4,23 +4,23 @@ import request from 'requestV2';
 // Ive been working on this hell all day put me out of my misery
 if (Settings.cmd_bookmark) register('command', (player, target) => {
 	if (player && player.startsWith('-r')) { // this is bullshit but it doesn't work otherwise
-		let v = U.popFile('resources/bookmark.json', 'id', target)
-		if (v==200) {ChatLib.chat('&cSuccessfully removed this bookmark!')} else {ChatLib.chat('&cSomething went wrong trying to perform this action!')}
+		let popResult = U.popFile('resources/bookmark.json', 'id', target)
+		if (popResult==200) {ChatLib.chat('&cSuccessfully removed this bookmark!')} else {ChatLib.chat('&cSomething went wrong trying to perform this action!')}
 		return;
 	}
 	if (player && player.match(/^[a-zA-Z0-9_]{2,16}$/mg)) {
 		request(`https://api.mojang.com/users/profiles/minecraft/${player}`)
-		.then((stuff) => {
-			let v = U.appendFile('resources/bookmark.json', {name: JSON.parse(stuff).name, id: JSON.parse(stuff).id})
-			if (v==400) return ChatLib.chat('&aThis house is already bookmarked!')
-			if (v==200) return ChatLib.chat('&aSuccessfully bookmarked this house!')	
+		.then((data) => {
+			let appendResult = U.appendFile('resources/bookmark.json', {name: JSON.parse(data).name, id: JSON.parse(data).id})
+			if (appendResult==400) return ChatLib.chat('&aThis house is already bookmarked!')
+			if (appendResult==200) return ChatLib.chat('&aSuccessfully bookmarked this house!')	
 		})
 	} else if (!player && U.inHousing(true)) {
 		const Bookmarks = JSON.parse(FileLib.read('ixMod', 'resources/bookmark.json'))
 		let BookmarkMsg = []
 		if (Bookmarks.length<1) return ChatLib.chat('&cYou have no bookmarks! Type /bookmark <player> to bookmark a house!')
 		ChatLib.chat(`&9&m${'-'.repeat(32)}`)
-		ChatLib.chat('&6            &6  Your Bookmarks')
+		ChatLib.chat('&6            &6  Your Bookmarks') // Spaces so it's centered properly
 		Bookmarks.forEach(v => {
 			let message = new Message()
 			message.addTextComponent(new TextComponent('&c ✖ ').setClick('run_command', `/bookmark -r ${v.id}`).setHover('show_text',`&cRemove bookmark "${v.name}"`))
